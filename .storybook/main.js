e30f2ba8a154225f6e6747f56921de9e892a62a9
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2022-11-10 20:40:19 +0000

    Fixed bug in Storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  core: {
+    builder: "webpack5"
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react"
+};
