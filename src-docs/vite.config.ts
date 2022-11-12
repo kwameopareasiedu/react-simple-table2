33b32c8bf01cb51d2c055df5afc1022a8d9bec09
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2022-11-12 11:46:32 +0000

    Defined base for Github pages in Vite config

--- src-docs/vite.config.ts
+++ src-docs/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-simple-table2/",
   server: {
     host: "127.0.0.1",
     port: 8001
