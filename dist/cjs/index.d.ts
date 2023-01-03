2c88073d98a883ecf330b7d214776192ef99d21e
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2023-01-03 13:32:13 +0000

    Added index.d.ts to cjs export

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,48 @@
+import { FC, HTMLAttributes } from "react";
+import {
+  CellResolver,
+  ObjectTableProps,
+  SimpleTableProps,
+  TableColumn,
+  TransformedTableColumn
+} from "../types";
+
+export declare const LinearProgressRoot: FC<HTMLAttributes<HTMLDivElement>>;
+
+export declare const LinearProgressBar: FC<HTMLAttributes<HTMLDivElement>>;
+
+export declare const SimpleTable: FC<SimpleTableProps<any>>;
+
+export declare const ObjectTable: FC<ObjectTableProps<any>>;
+
+export declare const Table: FC<HTMLAttributes<HTMLTableElement>>;
+
+export declare const Thead: FC<HTMLAttributes<HTMLTableSectionElement>>;
+
+export declare const Tbody: FC<HTMLAttributes<HTMLTableSectionElement>>;
+
+export declare const Tr: FC<HTMLAttributes<HTMLTableRowElement>>;
+
+export declare const Th: FC<HTMLAttributes<HTMLTableHeaderCellElement>>;
+
+export declare const Td: FC<HTMLAttributes<HTMLTableCellElement>>;
+
+export declare const breakpoints: {
+  xs: number;
+  sm: number;
+  md: number;
+  lg: number;
+  xl: number;
+  xxl: number;
+};
+
+export declare const resolveCellValue: (
+  item: any,
+  resolver: CellResolver,
+  itemIndex: number
+) => any;
+
+export declare const transformColumns: (
+  cols: Array<TableColumn>,
+  windowWidth: number
+) => Array<TransformedTableColumn>;
